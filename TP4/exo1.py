988caf80bbe8da7c80de6e05e1cd971dcd1e9172
spelaezmejia-gif <spelaezmejia@gmail.com>
2025-10-28 13:59:55 +0100

    Update exo1.py

--- TP4/exo1.py
+++ TP4/exo1.py
@@ -1,35 +1,38 @@
 import math
 import os
 def extraire_pi():
+    """Nettoyage des élements de ponctuation"""
     with open('poeme.txt') as f:
         contenu = f.read()
         texte_nettoye = contenu.replace(",", " ").replace(".", " ").replace("'", " ") \
             .replace("?", " ").replace("!", " ").replace("\n", " ")
-        #Nettoyage des élements de ponctuation
+
+        """Sauvegarde des mots sans ponctuation dans des chaines de caractères individuelles"""
         mots = texte_nettoye.split(" ")
-        #sauvegarde des mots sans ponctuation dans des chaines de caractères individuelles
+
         mots_propres=[]
+        """Nettoyage des mots vides qui peuvent alterer le programme"""
         for m in mots:
             if m != "":
                 mots_propres.append(m)
-        #Nettoyage des mots vides qui peuvent alterer le programme
         pi = float(0)
         i = 0
+        """Écriture de pi en utilisant les puissances de 10"""
         for m in mots_propres:
             pi += float(len(m)%10) * (10.0 ** float(i))
             i -= 1
-        #Écriture de pi en utilisant les puissances de 10
+
     return (pi)
 def creer_fichier_pi():
+    """Création du fichier qui va contenir pi"""
     with open('pi.txt','x'):
         pass
-#création du fichier qui va contenir pi
 
 
 def ecrire_pi():
+    """Écriture de pi dans le fichier crée"""
     with open("pi.txt",'w') as f:
         f.write(str(extraire_pi()))
-#écriture de pi dans le fichier crée
 
 
 def main():
